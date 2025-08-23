b1fb9591c26354ca5ad57238115b1249e0165786
Ayush Basera <baseralucky5@gmail.com>
2025-08-23 18:16:04 +0530

    added requirements.txt and also added admin facilities

--- quizApp/admin.py
+++ quizApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Test, Question, StudentTest, StudentAnswer
 
+admin.site.register(User)
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(StudentTest)
+admin.site.register(StudentAnswer)
 # Register your models here.
